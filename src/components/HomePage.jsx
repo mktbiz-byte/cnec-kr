23e6fb1ea68390980d133874707b0dc9d87cdf12
mktbiz-byte <234577369+mktbiz-byte@users.noreply.github.com>
2025-11-17 01:26:45 -0500

    Fix currency symbol from JPY to KRW

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -41,9 +41,9 @@ const HomePage = () => {
   }
 
   const formatCurrency = (amount) => {
-    return new Intl.NumberFormat('ja-JP', {
+    return new Intl.NumberFormat('ko-KR', {
       style: 'currency',
-      currency: 'JPY'
+      currency: 'KRW'
     }).format(amount || 0)
   }
 
